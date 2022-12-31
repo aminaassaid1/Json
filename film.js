529b4787a7b0a14e515ad4e2ab2662790e11d40f
aminaassaid1 <aminaassaid123@gmail.com>
2022-12-31 14:27:09 +0100

    new design

--- film.js
+++ film.js
@@ -1,16 +1,16 @@
-let xhr = new XMLHttpRequest(); 
+let xhr = new XMLHttpRequest();
 xhr.open("GET", "film.json", true);
-xhr.onload = function(){
-        let data = JSON.parse(xhr.response);
-        console.log(data)
-        
-        data.forEach(element => {
-            let tr = document.createElement("tr");
-        let poster= document.createElement("img");
+xhr.onload = function () {
+    let data = JSON.parse(xhr.response);
+    console.log(data)
+
+    data.forEach(element => {
+        let tr = document.createElement("tr");
+        let poster = document.createElement("img");
         let title = document.createElement("td");
         let RDate = document.createElement("td");
         let director = document.createElement("td")
-        let lasts = document.createElement("td"); 
+        let lasts = document.createElement("td");
         let festival = document.createElement("td");
         let actor = document.createElement("td")
         poster.append(element.poster);
@@ -20,8 +20,8 @@ xhr.onload = function(){
         lasts.append(element.lasts);
         festival.append(element.festival);
         actor.append(element.actor);
-        poster.setAttribute("src",element.poster)
-        poster.style.width = "100px"              
+        poster.setAttribute("src", element.poster)
+        poster.style.width = "100px"
         tr.appendChild(poster);
         tr.appendChild(title);
         tr.appendChild(RDate);
@@ -29,7 +29,7 @@ xhr.onload = function(){
         tr.appendChild(lasts);
         tr.appendChild(festival);
         tr.appendChild(actor);
-    
+
         document.querySelector('#tbody').appendChild(tr)
     });
 };
